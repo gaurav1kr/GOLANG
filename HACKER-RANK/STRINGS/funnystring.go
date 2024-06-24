f54fa8aa2ac9bbe0d6d25ecd74df15a9f4a4d846
Gaurav Kumar <sinha4545@gmail.com>
2024-06-24 20:31:14 +0530

    funny string - HACKER-RANK

--- HACKER-RANK/STRINGS/funnystring.go
+++ HACKER-RANK/STRINGS/funnystring.go
@@ -1,8 +1,11 @@
 // https://www.hackerrank.com/challenges/funny-string/problem?isFullScreen=true
+package main
+
 import (
 	"fmt"
 	"math"
 )
+
 func funnyString(s string) string {
 	// Write your code here
 	slen := len(s)
@@ -10,16 +13,16 @@ func funnyString(s string) string {
 	var diff2 float64
 	if slen > 2 {
 		for i := 0; i < slen/2; i++ {
-			if(s[i] > s[i+1]){
-				diff1= math.Abs(float64(s[i] - s[i+1]))
-			} else{
-				diff1= math.Abs(float64(s[i+1] - s[i]))
+			if s[i] > s[i+1] {
+				diff1 = math.Abs(float64(s[i] - s[i+1]))
+			} else {
+				diff1 = math.Abs(float64(s[i+1] - s[i]))
 			}
 
-			if(s[slen-1-i] > s[slen-2-i]){
-				diff2= math.Abs(float64(s[slen-1-i] - s[slen-2-i]))
-			} else{
-				diff2= math.Abs(float64(s[slen-2-i] - s[slen-1-i]))
+			if s[slen-1-i] > s[slen-2-i] {
+				diff2 = math.Abs(float64(s[slen-1-i] - s[slen-2-i]))
+			} else {
+				diff2 = math.Abs(float64(s[slen-2-i] - s[slen-1-i]))
 			}
 			if diff1 != diff2 {
 				return "Not Funny"
